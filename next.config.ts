8dc59de1eac57c6b50fb2b9b6abef5b9b2aa7822
Akash Gupta <akash.g6787@gmail.com>
2026-02-02 05:21:06 +0530

    resolved paging issue

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
+        protocol: "https",
+        hostname: "images.unsplash.com",
       },
       {
-        protocol: 'https',
-        hostname: 'i.pravatar.cc',
+        protocol: "https",
+        hostname: "i.pravatar.cc",
       },
     ],
   },
